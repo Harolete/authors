5407aac6e2ed0ce42c1f625c3c1b6877d45a1260
Harold_Pestano <pestanoh@globalhitss.com>
2021-11-15 22:35:50 -0300

    controller store video 14

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use App\Traits\ApiResponder;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class AuthorController extends Controller
 {
     use ApiResponder;
+
     /**
      * Create a new controller instance.
      *
@@ -21,29 +24,39 @@ class AuthorController extends Controller
 
     /**
      * Retorna una lista de autores
-     * @return  Illuminate\Http\Response
-     *
+     * @return JsonResponse
      */
     public function index()
     {
-
+        return $this->successResponse(Author::all());
     }
 
     /**
      * Crea una instancia de Author
      * @param Request $request
-     * @return  Illuminate\Http\Response
-     *
+     * @return JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request)
     {
+        $rules = [
+            'name'=>'required|max:255',
+            'gender'=>'required|max:255|in:male,female',
+            'country'=>'required|max:255'
+        ];
+
+        $this->validate($request, $rules);
+
+        $author = Author::create($request->all());
+
+        return $this->successResponse($author, Response::HTTP_CREATED);
 
     }
 
     /**
      * Retorna un author especifico
      * @param  $author
-     * @return  Illuminate\Http\Response
+     * @return lluminate\Http\Response
      */
     public function show($author)
     {
